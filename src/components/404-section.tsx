ed2d09c51c025c2e8b39ef2ed12b0c50435968cc
Jade Hernandez <jade.hernandez88@gmail.com>
2025-03-31 17:08:42 +0200

    refactor 404-section component and clean up layout

--- src/components/404-section.tsx
+++ src/components/404-section.tsx
@@ -7,23 +7,23 @@ const NotFoundSection = () => {
     /* Background Image */
     <section className="flex flex-col justify-center  h-screen bg-[url('/images/abstract-background.jpg')] bg-cover  bg-center">
       {/* Wrapper */}
-      <div className=" p-24 flex flex-col border border-red-500">
+      <div className=" p-24 flex flex-col">
         {/* Text & Button */}
-        <div className="flex flex-col p-24 gap-16 border border-blue-500 ">
+        <div className="flex flex-col p-24 gap-16">
           {/* Text pair */}
-          <div className="flex flex-col gap-6 border border-green-500">
+          <div className="flex flex-col gap-6">
             {/* Header */}
-            <div className="flex flex-col gap-3 border border-red-500">
+            <div className="flex flex-col gap-3">
               <span className="text-indigo-700 font-semibold text-base">Not found</span>
               <h1 className="font-semibold text-neutral-900 text-6xl">We can’t find the page</h1>
             </div>
             {/* Text */}
-            <div className="flex flex-col border border-orange-500">
+            <div className="flex flex-col">
               <span className="text-neutral-600 text-xl">Sorry, the page you are looking for doesn't exist or has been moved.</span>
             </div>
           </div>
           {/* button */}
-          <div className="flex border border-purple-500">
+          <div className="flex">
             <Button
               size={"lg"}
               variant={"primary"}

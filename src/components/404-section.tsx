47922e59b2cc29edf941bdc1c0a4f8e5790b57b1
Jade Hernandez <jade.hernandez88@gmail.com>
2025-04-01 16:02:44 +0200

    enhance layout and styling of 404-section component

--- src/components/404-section.tsx
+++ src/components/404-section.tsx
@@ -5,23 +5,23 @@ import { Button } from "./ui/button";
 const NotFoundSection = () => {
   return (
     /* Background Image */
-    <section className="flex flex-col justify-center  h-screen bg-[url('/images/abstract-background.jpg')] bg-cover  bg-center">
+    <section className="flex flex-col justify-center  h-screen bg-[url('/images/abstract-background.jpg')] bg-cover bg-center md:py-[302px] lg:py-[105px] lg:px-24 border border-black">
       {/* Wrapper */}
-      <div className=" lg:p-24 flex flex-col p-5">
+      <div className=" flex flex-col px-3 py-12 gap-12 border md:px-4 md:py-16 md:gap-8 border-red-500">
         {/* Text & Button */}
-        <div className="flex flex-col lg:p-24 gap-16 md:py-16 md:px-4">
+        <div className="flex flex-col gap-8 lg:p-24 md:gap-16  border border-green-400">
           {/* Text pair */}
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-6 border border-blue-500">
             {/* Header */}
-            <header className="flex flex-col gap-3">
+            <header className="flex flex-col gap-3 border border-yellow-300">
               <span className="text-indigo-700 font-semibold text-base">Not found</span>
-              <h1 className="font-semibold text-neutral-900 text-4xl md:text-6xl">We can’t find the page</h1>
+              <h1 className="font-semibold text-neutral-900 text-4xl md:text-5xl lg:text-6xl">We can’t find the page</h1>
             </header>
             {/* Text */}
             <p className="text-neutral-600 md:text-xl text-lg">Sorry, the page you are looking for doesn&apos;t  exist or has been moved.</p>
           </div>
           {/* button */}
-          <div className="flex">
+          <div className="flex border border-purple-500">
             <Button
               className="w-full gap-[6px] px-5 py-3 text-base items-center justify-center md:gap-[10px] md:px-6 md:py-4 md:text-lg rounded md:w-auto"
               variant={"primary"}

839e1cf9c5965416310238708e5e2637f069e351
Jade Hernandez <jade.hernandez88@gmail.com>
2025-04-02 13:55:46 +0200

    style: adjust padding in 404-section component

--- src/components/404-section.tsx
+++ src/components/404-section.tsx
@@ -11,7 +11,7 @@ const NotFoundSection = () => {
       {/* Wrapper */}
       <div className=" flex flex-col px-3 py-12 gap-12 md:px-4 md:py-16 md:gap-8">
         {/* Text & Button */}
-        <div className="flex flex-col gap-8 lg:p-24 md:gap-16 ">
+        <div className="flex flex-col gap-8  md:gap-16 ">
           {/* Text pair */}
           <div className="flex flex-col gap-6 ">
             {/* Header */}
